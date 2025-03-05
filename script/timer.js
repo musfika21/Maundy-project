1d2f34028f75f2205995e8d0b938ecfa6aa760bc
Musika Iqfat <musfikaiqfatmomo21.com>
2025-03-05 22:31:56 +0600

    first commit

---
+++ script/timer.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+    const targetDate = new Date('March 10, 2025 00:00:00').getTime();
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
